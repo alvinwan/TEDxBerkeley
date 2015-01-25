f575c0be0800ad83be528461de001f516c9d6461
Alvin Wan <hi@alvinwan.com>
2015-01-25 08:50:13 -0800

    Added logo to homepage
    fixed positions

--- iOSApp/TimelineTableViewController.swift
+++ iOSApp/TimelineTableViewController.swift
@@ -13,7 +13,7 @@ class TimelineTableViewController: UITableViewController, UITableViewDelegate, U
 
     @IBOutlet var TimelineView: UITableView!
     
-    var items = ["8:00 a.m. - Booths open.", "9:00 a.m. - \"The Earth is actually a Cube\" - Arnold Schwargeasldfhli", "12:00 p.m. - Lunch Break starts", "1:00 p.m. \"Why Water is Brown\" - John Doe", "4:00 p.m. - Event Conclusion."]
+    var items = ["8:00 a.m. - Booths open.", "9:00 a.m. - 'The Earth is actually a Cube' - Arnold Schwargeasldfhli", "12:00 p.m. - Lunch Break starts", "1:00 p.m. 'Why Water is Brown' - John Doe", "4:00 p.m. - Event Conclusion."]
     
     override func viewDidLoad() {
         super.viewDidLoad()
